09de1f23e16b1b6289cc36605cb318768a72a173
Felix Stålö <felix.stalo@elev.ga.ntig.se>
2022-09-08 19:35:36 +0200

    Blind-simulator

--- wallakmannende23/Program.cs
+++ wallakmannende23/Program.cs
@@ -1,41 +1,133 @@
-﻿//läser in namet
-String Way = "";
+﻿string currentRoom = "Hall";
 
-while(Way !="Vänster" && Way !="Höger" && Way!="Frammåt")
+string direction = "";
+
+while (currentRoom != "end")
 {
-    Console.WriteLine("Vilket väg kommer du ta");
-
-Way = Console.ReadLine();
-
-Console.WriteLine($"Ok, du väljer att gå åt {Way}!");
-
-    if(Way == "Vänster")
+    if (currentRoom == "Hall")
     {
-        Console.WriteLine("Du hamnar till en ny korsning var kommer du gå!");
-    }
-    else if(Way=="Högrer"){
-        Console.WriteLine("du väljer att gå åt höger och hamnar i en ny korsning");
-    }
-    else if(Way=="Frammåt"){
-    Console.WriteLine("Du väljer att gå frammåt rätt genom mörkret tills du kommer till en liten större korsning");
-    Console.WriteLine("Vart ska du gå nu?");
+        Console.WriteLine("Du är i hallen");
+
+        direction = Console.ReadLine();
+
+        if (direction == "Vänster")
+        {
+            currentRoom = "Toaletten";
+        }
+        else if (direction == "Höger")
+        {
+            currentRoom = "Köket";
+        }
+        else
+        {
+            Console.WriteLine("Du går in i väggen Ouch!");
+        }
     }
     
-
-    if(Way == "Frammåt")
+    else if (currentRoom == "Köket")
     {
-      Console.WriteLine("Du vandrar genom mörkret innan du kommer till en ny korsning");
+        Console.WriteLine("Du är i köket");
+
+        direction = Console.ReadLine();
+
+        if (direction == "Vänster")
+        {
+            currentRoom = "Kontoret";
+        }
+        else if (direction == "quickwayout")
+        {
+            currentRoom = "end";
+            Console.WriteLine("du har lyckats fly");
+        }
+        else if(direction=="backa")
+        {
+            currentRoom = "hall";  
+        }
+        else
+        {
+            Console.WriteLine("Du går in i väggen Ouch!");
+        }
     }
+       else if (currentRoom == "Toaletten")
+    {
+        Console.WriteLine("Du är i inne på toaletten");
 
+        direction = Console.ReadLine();
+
+        if (direction == "backa")
+        {
+            currentRoom = "hall";
+        }
+        else
+        {
+            Console.WriteLine("Du går in i väggen Ouch!");
+        }
+    }
+        else if(currentRoom == "Kontoret")
+        {
+            if(direction == "Vänster")
+            {
+                Console.WriteLine("du går genom en nödutgång och kommer ut ur huset");
+                currentRoom = "end";
+            }
+        else
+        {
+            Console.WriteLine("Du går in i väggen Ouch!");
+        }
+        }
+        
+
+
+
+
+
+
+
+}
+
+
+
+
+/*
+
+//läser in namet
+String Way = "";
+
+
+
+{
+    while (Way != "Vänster" && Way != "Höger" && Way != "Frammåt")
+    {
+        Console.WriteLine("Vilket väg kommer du ta");
+
+        Way = Console.ReadLine();
+
+        Console.WriteLine($"Ok, du väljer att gå åt {Way}!");
+
+        if (Way == "Vänster")
+        {
+            Console.WriteLine("Du hamnar till en ny korsning var kommer du gå!");
+
+        }
+        else if (Way == "Högrer")
+        {
+            Console.WriteLine("du väljer att gå åt höger och hamnar i en ny korsning");
+
+        }
+        else if (Way == "Frammåt")
+        {
+            Console.WriteLine("Du väljer att gå frammåt rätt genom mörkret tills du kommer till en liten större korsning");
+            Console.WriteLine("Vart ska du gå nu?");
+
+        }
 
 
 
     }
+}
 
 
-
-
-Console.ReadLine();
+*/
 
 
 
